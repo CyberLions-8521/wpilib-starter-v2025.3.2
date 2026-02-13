5133824d12e05339106fada3058f2513eb6156a5
RamenNoodling <145399261+RamenNoodling@users.noreply.github.com>
2026-02-12 17:28:29 -0800

    part1.java
    created motor and set speed

--- xrp-timed-template/src/main/java/frc/robot/Robot.java
+++ xrp-timed-template/src/main/java/frc/robot/Robot.java
@@ -19,7 +19,8 @@ import frc.robot.VexV5Controller;
 
 
 public class Robot extends TimedRobot {
-
+  XRPMotor m_leftMotor = new XRPmotor(0); /*creates XRPMotor object*/
+  
   public Robot() {}
 
   @Override
@@ -32,6 +33,7 @@ public class Robot extends TimedRobot {
   public void teleopPeriodic()
   {
     // type code in here - runs forever
+    m_leftMotor.set(0.5) //sets to half speed
   }
 
 
